0d7f036021556d3dc108ea890cdb0f76b5fff165
rahul <=rahulrajyadav2014@gmail.com>
2017-12-30 13:08:42 +0530

    Shopping website created

---
+++ includes/common.php
@@ -0,0 +1,5 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "lifestylestore")or die($mysqli_error($con));
+session_start();
+
+?>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All Rights Reserved  |  Contact Us: +91 980425 9957</p>	
+        </center>
+    </div>
+</footer>
